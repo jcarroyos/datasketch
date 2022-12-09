79e3f61a3a76bd27f874ecc1e4d7c33ee573fa56
jcarroyos <jcarroyos@gmail.com>
2022-12-09 00:19:13 -0500

    display profile picture as img

--- front/src/components/TableBody.js
+++ front/src/components/TableBody.js
@@ -6,6 +6,16 @@ const TableBody = ({ tableData, columns }) => {
           <tr key={data.id}>
             {columns.map(({ accessor }) => {
               const tData = data[accessor] ? data[accessor] : "——";
+              let condition = tData.toString().substring().slice(-4);
+              if ( condition  === '.jpg' || condition  === '.png' ) {
+                return (
+                    <td key={accessor}>
+                        <figure>
+                            <img className="avatar" alt={data.nickname+"'s avatar"} key={accessor} src={tData}/>
+                        </figure>
+                    </td>
+                );
+              }
               return <td key={accessor}>{tData}</td>;
             })}
           </tr>
